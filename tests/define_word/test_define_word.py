a0d0cd7972f79f0a579d4030ab8b03bb6142c914
michael-sankur <mdsankur@gmail.com>
2025-01-13 20:35:04 -0800

    Remove old tests with cleanup method. Remove cleanup method.

--- tests/define_word/test_define_word.py
+++ tests/define_word/test_define_word.py
@@ -12,22 +12,6 @@ class DefineWordResult(TaskOutput):
     result: str
 
 
-# @pytest.mark.asyncio
-# async def test_define_word() -> None:
-#     flow = Flow.load(
-#         name="define_word_flow",
-#         response_models=[DefineWordResult],
-#         flows_spec_path=Path("tests/define_word/flow.yaml"),
-#         tasks_spec_path=Path("tests/define_word/tasks.yaml"),
-#     )
-#     await flow.run(run_id="test_define_word", word_to_define=WORD_TO_DEFINE)
-#     task_output = flow.get("define_word_task", response_model=DefineWordResult)
-
-#     print(task_output.result)
-
-#     assert WORD_TO_DEFINE.lower() in task_output.result.lower()
-
-
 @pytest.mark.asyncio
 async def test_define_word() -> None:
     flow = Flow.load(
@@ -36,12 +20,9 @@ async def test_define_word() -> None:
         flows_spec_path=Path("tests/define_word/flow.yaml"),
         tasks_spec_path=Path("tests/define_word/tasks.yaml"),
     )
-    try:
-        await flow.run(run_id="test_define_word", word_to_define=WORD_TO_DEFINE)
-        task_output = flow.get("define_word_task", response_model=DefineWordResult)
+    await flow.run(run_id="test_define_word", word_to_define=WORD_TO_DEFINE)
+    task_output = flow.get("define_word_task", response_model=DefineWordResult)
 
-        print(task_output.result)
+    print(task_output.result)
 
-        assert WORD_TO_DEFINE.lower() in task_output.result.lower()
-    finally:
-        await flow.cleanup()
+    assert WORD_TO_DEFINE.lower() in task_output.result.lower()
--- tests/simple_addition/test_simple_addition.py
+++ tests/simple_addition/test_simple_addition.py
@@ -10,19 +10,6 @@ class AdditionResult(TaskOutput):
     result: str
 
 
-# @pytest.mark.asyncio
-# async def test_simple_addition() -> None:
-#     flow = Flow.load(
-#         name="simple_addition",
-#         response_models=[AdditionResult],
-#         flows_spec_path=Path("tests/simple_addition/flow.yaml"),
-#         tasks_spec_path=Path("tests/simple_addition/tasks.yaml"),
-#     )
-#     await flow.run(run_id="test_simple_addition", num1="1", num2="2")
-#     task_output = flow.get("add_numbers", response_model=AdditionResult)
-
-#     assert task_output.result == "3"
-
 @pytest.mark.asyncio
 async def test_simple_addition() -> None:
     flow = Flow.load(
@@ -31,9 +18,8 @@ async def test_simple_addition() -> None:
         flows_spec_path=Path("tests/simple_addition/flow.yaml"),
         tasks_spec_path=Path("tests/simple_addition/tasks.yaml"),
     )
-    try:
-        await flow.run(run_id="test_simple_addition", num1="1", num2="2")
-        task_output = flow.get("add_numbers", response_model=AdditionResult)
-        assert task_output.result == "3"
-    finally:
-        await flow.cleanup()
+    await flow.run(run_id="test_simple_addition", num1="1", num2="2")
+    task_output = flow.get("add_numbers", response_model=AdditionResult)
+
+    assert task_output.result == "3"
+
