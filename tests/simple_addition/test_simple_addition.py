c6fafea7ff3e1454eb88601640f0e9bb59f139a5
michael-sankur <mdsankur@gmail.com>
2025-01-21 13:57:39 -0800

    Conditional addition with split tasks working.

--- tests/simple_addition/test_simple_addition.py
+++ tests/simple_addition/test_simple_addition.py
@@ -6,20 +6,20 @@ from vers.flow import Flow
 from vers.types.tasks import TaskOutput
 
 
-class AdditionResult(TaskOutput):
+class SimpleAdditionResult(TaskOutput):
     result: str
 
 
 @pytest.mark.asyncio
 async def test_simple_addition() -> None:
     flow = Flow.load(
-        name="simple_addition",
-        response_models=[AdditionResult],
+        name="simple_addition_flow",
+        response_models=[SimpleAdditionResult],
         flows_spec_path=Path("tests/simple_addition/flow.yaml"),
         tasks_spec_path=Path("tests/simple_addition/tasks.yaml"),
     )
     await flow.run(run_id="test_simple_addition", num1="1", num2="2")
-    task_output = flow.get("add_numbers", response_model=AdditionResult)
+    task_output = flow.get("simple_addition_task", response_model=SimpleAdditionResult)
 
     assert task_output.result == "3"
 
