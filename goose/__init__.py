4f8b61960c27b6206869ccf2ed4a7dc77d45159a
Nash Taylor <nashtaylor22@gmail.com>
2025-02-05 13:03:31 -0800

    fix exports

--- goose/__init__.py
+++ goose/__init__.py
@@ -1,4 +1,6 @@
-from goose._internal.agent import Agent
-from goose._internal.flow import flow, FlowArguments
-from goose._internal.result import Result, TextResult
-from goose._internal.task import task
+from ._internal.agent import Agent
+from ._internal.flow import FlowArguments, flow
+from ._internal.result import Result, TextResult
+from ._internal.task import task
+
+__all__ = ["Agent", "flow", "FlowArguments", "Result", "TextResult", "task"]
